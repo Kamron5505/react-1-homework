e37333e01d7d5dc9a1416225ac79aca47f6d1dfa
Kamron5505 <kama58077@gmail.com>
2025-07-22 14:08:27 +0500

    hello react

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,36 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
   return (
     <>
-    
-      
-    </>
-  )
-}
+      <h1>Welcome to React JS</h1>
+      <h2>This is my first React app</h2>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>Javascript</li>
+        <li>React</li>
+      </ul>
 
-export default App
+      <h3>What is React</h3>
+      <p>
+        React nima? React — bu Facebook tomonidan yaratilgan ochiq kodli
+        JavaScript kutubxonasi bo‘lib, asosan foydalanuvchi interfeyslarini (UI)
+        yaratish uchun mo‘ljallangan. U yagona sahifali ilovalar (single-page
+        applications) va dinamik, sezgir veb-ilovalarni ishlab chiqishda keng
+        qo‘llaniladi.
+      </p>
+
+      <h3>What is Single-page application(SPA)</h3>
+      <p>
+        SPA (Single Page Application) — bu bitta sahifali veb-ilova bo‘lib, unda
+        sahifa kontenti dinamik ravishda yangilanadi va foydalanuvchining har
+        bir amalida butun sahifani qaytadan yuklash talab qilinmaydi. JavaScript
+        yordamida faqat kerakli kontent o‘zgaritiladi, bu esa ilovani tezkor va
+        foydalanuvchi uchun qulay qiladi.
+      </p>
+    </>
+  );
+};
+
+export default App;
